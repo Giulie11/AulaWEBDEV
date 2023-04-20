164c0c28ade6ec99f8f685c6b20406dd42e13dec
Giulie11 <121198320+Giulie11@users.noreply.github.com>
2023-04-20 16:48:39 +0200

    Add files via upload
    Test 20-04-2023

---
+++ test 20-04-2023/script.js
@@ -0,0 +1,35 @@
+// file JS (linguaggio di prgrammazione) per la logica della pag. web (es interazione utente,
+// comunicazione con server esterni...),
+
+
+// Domanda n3: elementi prinicpali in Javascript */
+// Variabili e costanti (contenitori per inserimento dei dati)
+const saluto = "Ciao";
+let nome = "Mirko";
+
+// Metodi e funzioni (mi permettono di fare calcoli per esempio)
+console.log(saluto + " " + nome); 
+
+// Operatori (LOGICI,    ARITMETICI,    CONFRONTO)
+//             &&,||,!  +,-,/,*,%      ==, !=, <, >
+
+//Iterazione - cicli (for, while, do-while)
+//Selezione (if, if-else, switch)
+
+
+
+//Domanda n 4: Tipizzazione rigida e tipizzazione debole
+// Tipizzazione rigida abbiamo bisogno di specificare il tipo di dato che inseriamo 
+// all'interno della variabile che stiamo dichiarando (per esempio in Java) 
+// mentre in quella debole (come in JS) abbiamo solo due tipi di variabili 
+//(var(quasi deprecata) e let e const)
+// all'interno delle quali possiamo mettere tutti i tipi di dato, perchè il tipo viene
+// automaticamente assegnato in base al contenuto della variabile. 
+
+//let numero = 12;      int numero = 12;
+//let parola = "Ciao";  String parola = "Ciao";
+
+
+
+//Domanda n 5: Punto di collegamento tra back-end e front-end:
+// API (interfaccia che mi permette di comunicare tra front-end e back-end)
